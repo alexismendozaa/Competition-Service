80cea0b1324ab3784701104d706503274a8d971c
alexismendozaa <afmendozaf@uce.edu.ec>
2025-06-28 15:20:06 -0500

    🚧 wip: working on new feature

--- select-competition-ms/run.py
+++ select-competition-ms/run.py
@@ -2,5 +2,4 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    # Cambiar el puerto a 3024
-    app.run(host="0.0.0.0", port=3024, debug=True)  # Aquí cambiamos el puerto a 3024
+    
--- start-competition-ms/run.py
+++ start-competition-ms/run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    # Establecer el puerto directamente en el código
-    app.run(host="0.0.0.0", port=3023, debug=True)  # Aquí establecemos el puerto 3023
+   
+    app.run(host="0.0.0.0", port=3023, debug=True) 
